549d217a0b2c11582b8f577f21fae442f6530c7d
unknown <aliiobeid01@gmail.com>
2022-09-23 23:02:07 +0300

    fetch the politics api,tested

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const card_last=document.getElementById('card_last');
 const card_sports=document.getElementById('card_sports');
+const card_politics=document.getElementById('card_politics');
     
     //fetch the latest news api with jquery method
     $.get("http://localhost/news-bootstrap5/api/latest_news.php", function(data,status) {  
@@ -25,6 +26,24 @@ const card_sports=document.getElementById('card_sports');
       console.log(data);  
       for(let i=0;i<data.length;i++){
         card_sports.innerHTML+=
+       `<div class="col-sm-3">
+         <div class="card">
+           <img src="${data[i].image}" class="card-img-top" alt="...">
+           <div class="card-body">
+             <h5 class="card-title text-danger">${data[i].title}</h5>
+             <p class="card-text">${data[i].breif}</p>
+             <a href="#" class="btn btn-danger">READ</a>
+           </div>
+         </div>
+       </div>
+       `
+      } 
+  });
+     //fetch the politics news api with jquery method
+     $.get("http://localhost/news-bootstrap5/api/politics_news.php", function(data,status) {  
+      console.log(data);  
+      for(let i=0;i<data.length;i++){
+        card_politics.innerHTML+=
        `<div class="col-sm-3">
          <div class="card">
            <img src="${data[i].image}" class="card-img-top" alt="...">
