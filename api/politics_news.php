212dd5ac52ed3301933776756df342510f7bb426
unknown <aliiobeid01@gmail.com>
2022-09-23 22:44:58 +0300

    add api to get the news of category politics

---
+++ api/politics_news.php
@@ -0,0 +1,23 @@
+<?php
+//get the politics news from the table news
+header('Access-Control-Allow-Origin: http://localhost/');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header("Content-type:application/json");
+
+include("connection.php");
+
+
+$query = $mysqli->prepare("SELECT id,title,image,breif,date_time from news where category='politics'");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($info = $array->fetch_assoc()){
+    $response[] = $info;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
