e048f6b34e2bd6622dd77870cfce393206d124bc
unknown <aliiobeid01@gmail.com>
2022-09-23 23:34:56 +0300

    add api to get the full news when click on read

---
+++ api/readnews.php
@@ -0,0 +1,24 @@
+<?php
+//get the news from the table news by id
+header('Access-Control-Allow-Origin: http://localhost/');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header("Content-type:application/json");
+
+include("connection.php");
+
+$id=$_POST['id'];
+
+$query = $mysqli->prepare("SELECT * from news where id=$id");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($info = $array->fetch_assoc()){
+    $response[] = $info;
+}
+
+$json = json_encode($response);
+echo $json;
+
+?>
