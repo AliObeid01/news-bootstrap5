5cb986f0327597d63dc945f1a460fc264def2f06
unknown <aliiobeid01@gmail.com>
2022-09-23 23:52:07 +0300

    add readnews script to fetch read single news api

---
+++ js/readnews.js
@@ -0,0 +1,23 @@
+const readnew=document.getElementById('readnew');
+const params = new URLSearchParams(window.location.search)
+const id=params.get('id');
+
+
+    //fetch the a single new api with jquery method
+    $.get(`http://localhost/news-bootstrap5/api/readnews.php?id=${id}`, function(data,status) {  
+        readnew.innerHTML=
+        `<img src="${data[0].image}" class="card-img-top"></img>
+         <div class="card-header">
+         ${data[0].title}
+         </div>
+         <div class="card-body">
+           <h5 class="card-title">${data[0].breif}</h5>
+           <p class="card-text">${data[0].text}</p>
+           
+         </div>
+         <div class="card-footer text-muted">
+         ${data[0].author},${data[0].date_time}
+         </div>
+         `
+        
+    });
